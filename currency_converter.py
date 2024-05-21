90a1f36d76d0371e83d16e36b6db545d0d6aec23
Joshua White <91299772+keonu94@users.noreply.github.com>
2024-05-20 21:52:02 -0700

    Create currency_converter.py

---
+++ currency_converter.py
@@ -0,0 +1,47 @@
+import requests
+import time
+import zmq
+
+
+def convert_currency(amount, from_currency, to_currency):
+    # Input 'api_key' with your actual API key from https://app.exchangerate-api.com/dashboard/
+    api_key = ''
+    url = f'https://v6.exchangerate-api.com/v6/{api_key}/latest/{from_currency}'
+
+    response = requests.get(url)
+    data = response.json()
+
+    if 'error' in data:
+        return None, data['error']
+
+    if to_currency not in data['conversion_rates']:
+        return None, f'Invalid currency code: {to_currency}'
+
+    exchange_rate = data['conversion_rates'][to_currency]
+    converted_amount = amount * exchange_rate
+
+    return converted_amount, None
+
+
+if __name__ == "__main__":
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("tcp://*:5555")
+
+    #  Wait for next request from client
+    message = socket.recv()
+    print("Received request: %s" % message)
+
+    #  Do some 'work'
+    time.sleep(1)
+    message = message.decode()
+    message = message.split()
+    message[0] = float(message[0])
+    converted_amount, error = convert_currency(message[0], message[1], message[2])
+    if error:
+        reply = "Conversion failed:", error
+    else:
+        reply = f"{message[0]} {message[1]} is equal to {converted_amount:.2f} {message[2]}"
+
+    #  Send reply back to client
+    socket.send(str.encode(reply))
