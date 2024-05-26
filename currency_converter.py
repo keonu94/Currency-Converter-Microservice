e95caa521e947e9855110ae07a28121a88fca907
Joshua White <91299772+keonu94@users.noreply.github.com>
2024-05-25 20:06:08 -0700

    Update currency_converter.py

--- currency_converter.py
+++ currency_converter.py
@@ -24,24 +24,25 @@ def convert_currency(amount, from_currency, to_currency):
 
 
 if __name__ == "__main__":
-    context = zmq.Context()
-    socket = context.socket(zmq.REP)
-    socket.bind("tcp://*:5555")
+    while True:
+        context = zmq.Context()
+        socket = context.socket(zmq.REP)
+        socket.bind("tcp://*:5555")
 
-    #  Wait for next request from client
-    message = socket.recv()
-    print("Received request: %s" % message)
+        #  Wait for next request from client
+        message = socket.recv()
+        print("Received request: %s" % message)
 
-    #  Do some 'work'
-    time.sleep(1)
-    message = message.decode()
-    message = message.split()
-    message[0] = float(message[0])
-    converted_amount, error = convert_currency(message[0], message[1], message[2])
-    if error:
-        reply = "Conversion failed:", error
-    else:
-        reply = f"{message[0]} {message[1]} is equal to {converted_amount:.2f} {message[2]}"
+        #  Do some 'work'
+        time.sleep(1)
+        message = message.decode()
+        message = message.split()
+        message[0] = float(message[0])
+        converted_amount, error = convert_currency(message[0], message[1], message[2])
+        if error:
+            reply = "Conversion failed:", error
+        else:
+            reply = f"{message[0]} {message[1]} is equal to {converted_amount:.2f} {message[2]}"
 
-    #  Send reply back to client
-    socket.send(str.encode(reply))
+        #  Send reply back to client
+        socket.send(str.encode(reply))
